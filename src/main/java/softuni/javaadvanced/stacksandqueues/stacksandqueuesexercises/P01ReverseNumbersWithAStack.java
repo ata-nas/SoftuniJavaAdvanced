f8e6ba57d08e5420d063b96f460c6ae7e2c76ab6
ATANAS ATANASOV <94196097+ata-nas@users.noreply.github.com>
2023-01-13 08:43:43 +0200

    Stacks and Queues Exercises added.

---
+++ src/main/java/softuni/javaadvanced/stacksandqueues/stacksandqueuesexercises/P01ReverseNumbersWithAStack.java
@@ -0,0 +1,20 @@
+package softuni.javaadvanced.stacksandqueues.stacksandqueuesexercises;
+
+import java.util.*;
+
+public class P01ReverseNumbersWithAStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Deque<String> stack = new ArrayDeque<>();
+
+        String[] numbers = sc.nextLine().split(" ");
+        for (String item : numbers) {
+            stack.push(item);
+        }
+
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
