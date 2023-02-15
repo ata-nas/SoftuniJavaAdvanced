99d1ab51313199780eb56b0f418117e3fd1c54ee
ATANAS ATANASOV <94196097+ata-nas@users.noreply.github.com>
2023-02-15 20:50:07 +0200

    Additional exercises.

--- src/main/java/softuni/advanced/iteratorsandcomparators/exercises/Collection/Main.java
+++ src/main/java/softuni/advanced/iteratorsandcomparators/exercises/Collection/Main.java
@@ -1,7 +1,5 @@
 package softuni.advanced.iteratorsandcomparators.exercises.Collection;
 
-import Collection.ListyIterator;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
